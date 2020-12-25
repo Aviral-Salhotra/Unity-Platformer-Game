ad432f7aa9faf3c45f9ce1784bf91ebfab6f814b
Aviral-Salhotra <aviralsalhotra2@gmail.com>
2020-12-24 20:38:24 -0800

    Added Player and Player Controller

---
+++ 2d-platformer/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RaycastController {
+    
+    public Vector3 move;
+
+    public override void Start() {
+        base.Start ();
+    }
+
+    void Update () {
+        Vector3 velocity = move * Time.deltaTime;
+        transform.Translate (velocity);
+    }
+}
